3295392cdf2b4cd8df0f24b817ea8c533d29e174
Yvonne Spitznagel <Yvonne.Spitznagel@generic.de>
2023-04-25 16:25:55 +0200

    Add test solution

---
+++ TDD-Squirrel.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TDD_Squirrel.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
