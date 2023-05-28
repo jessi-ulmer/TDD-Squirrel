465176eeb309b14882b3c4731d9e04b3d2876a04
Dorothea Kintz <dorothea.kintz@generic.de>
2023-05-28 20:51:40 +0200

    SetUp Tests for RollDiceAndMove

---
+++ TDD-Squirrel.Test/RollDiceAndMoveTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TDD_Squirrel.Test
+{
+    public class RollDiceAndMoveTests
+    {
+        [Test]
+        public void SetUpTest()
+        {
+            var result = true;
+            result.Should().BeTrue();  
+        }
+    }
+}
