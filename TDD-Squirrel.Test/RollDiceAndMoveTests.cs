28f686ba5940c55b81ec95c18d61834dddd33de0
Dorothea Kintz <dorothea.kintz@generic.de>
2023-05-29 16:55:19 +0200

    add RandomGeneratorFunctionTest to understand RandomNumber generator

--- TDD-Squirrel.Test/RollDiceAndMoveTests.cs
+++ TDD-Squirrel.Test/RollDiceAndMoveTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using FluentAssertions.Execution;
 
 namespace TDD_Squirrel.Test
 {
@@ -14,6 +15,29 @@ namespace TDD_Squirrel.Test
         {
             var result = true;
             result.Should().BeTrue();  
+        }
+
+        [Test]
+        public void RandomGeneratorFunctionTest()
+        {
+            var generator = new Random(0);
+            var result = generator.Next(7);
+            using var scope1 = new AssertionScope();
+            result.Should().BeInRange(1, 6);
+            result.Should().Be(5);
+            result = generator.Next(7);
+            result.Should().Be(5);
+            var results = new[] { generator.Next(7), generator.Next(7), generator.Next(7), generator.Next(7) };
+            using var scope2 = new AssertionScope();
+            results.Should().BeEquivalentTo(new[] { 5, 3, 1, 3 });
+
+            using var scope3 = new AssertionScope();
+            for (var i = 0; i < 100; i++)
+            {
+                result = generator.Next(1, 7);
+                result.Should().NotBe(0);
+            }
+
         }
     }
 }
