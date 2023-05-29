90f275b77aaea000b35e7710a3a9b1d7e4d2e567
Dorothea Kintz <dorothea.kintz@generic.de>
2023-05-29 17:12:38 +0200

    w.i.p add test for rolling one die
    not implemented, yet

--- TDD-Squirrel.Test/RollDiceAndMoveTests.cs
+++ TDD-Squirrel.Test/RollDiceAndMoveTests.cs
@@ -22,12 +22,14 @@ namespace TDD_Squirrel.Test
         {
             var generator = new Random(0);
             var result = generator.Next(7);
+
             using var scope1 = new AssertionScope();
             result.Should().BeInRange(1, 6);
             result.Should().Be(5);
             result = generator.Next(7);
             result.Should().Be(5);
             var results = new[] { generator.Next(7), generator.Next(7), generator.Next(7), generator.Next(7) };
+
             using var scope2 = new AssertionScope();
             results.Should().BeEquivalentTo(new[] { 5, 3, 1, 3 });
 
@@ -36,8 +38,17 @@ namespace TDD_Squirrel.Test
             {
                 result = generator.Next(1, 7);
                 result.Should().NotBe(0);
+                result.Should().BeInRange(1, 6);
             }
 
+        }
+
+        [Test]
+        public void RollDice_Should_Return_Integer_Between_1_And_6()
+        {
+            var result = FigureMover.RollDie();
+            result.Should().BeOfType<int>();
+            result.Should().BeInRange(1, 6);
         }
     }
 }
