d5d9c060cdd33c02ebf24aaa87fcb49734a7a9be
Dorothea Kintz <dorothea.kintz@generic.de>
2023-06-08 17:01:51 +0200

    red: add testcase for "game not finished, yet"

--- TDD-Squirrel.Test/PieceMoverTests.cs
+++ TDD-Squirrel.Test/PieceMoverTests.cs
@@ -71,6 +71,7 @@ namespace TDD_Squirrel.Test
         }
 
         [TestCase(9, true)]
+        [TestCase(0, false)]
         public void Move_Should_Return_DieStatus(int position, bool expected)
         {
             var result = PieceMover.Move(position);
