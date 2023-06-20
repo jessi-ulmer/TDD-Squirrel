ab00caec741d56722aa36e32f08b5927b1a3ef49
Dorothea Kintz <dorothea.kintz@generic.de>
2023-06-21 00:49:01 +0200

    red: use fakeiteasy to fake die result

---
+++ SnakesAndLaddersLibTests/DisableDiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using FluentAssertions;
+using SnakesAndLaddersLib;
+
+namespace SnakesAndLaddersLibTests
+{
+    public class DisableDiceTests
+    {
+
+        [Test]
+        public void PieceMover_Should_Return_FinishReached()
+        {
+            var diceRoller = A.Fake<IDiceRoller>();
+            A.CallTo(() => diceRoller.RollDie()).Returns(5);
+            var pieceMover = new PieceMover(diceRoller);
+            var movingResult = pieceMover.Move(5);
+            movingResult.Position.Should().Be(10);
+            movingResult.IsFinalSquareReached.Should().BeTrue();
+
+
+        }
+
+    }
+}
