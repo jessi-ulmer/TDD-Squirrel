ee0508c6a4c72250aaeb3a389c3d2743e2da4df1
Dorothea Kintz <dorothea.kintz@generic.de>
2023-06-22 21:49:17 +0200

    move tests for PieceMover into suitable file

--- SnakesAndLaddersLibTests/DisableDiceTests.cs
+++ SnakesAndLaddersLibTests/DisableDiceTests.cs
@@ -5,12 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using FakeItEasy;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using SnakesAndLaddersLib;
 
 namespace SnakesAndLaddersLibTests
 {
     public class DisableDiceTests
     {
+        private IDiceRoller _diceRoller;
+        private PieceMover _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _diceRoller = A.Fake<IDiceRoller>();
+            _sut = new PieceMover(_diceRoller);
+        }
 
         [Test]
         public void PieceMover_Should_Return_FinishReached()
@@ -21,8 +31,29 @@ namespace SnakesAndLaddersLibTests
             var movingResult = pieceMover.Move(5);
             movingResult.Position.Should().Be(10);
             movingResult.IsFinalSquareReached.Should().BeTrue();
+        }
 
+        [TestCase(0, 1, 1)]
+        [TestCase(5, 2, 7)]
+        [TestCase(5, 5, 10)]
+        [TestCase(7, 6, 10)]
+        public void Move_Should_Return_PositionInRange(int previousPosition, int fakedDie, int expected)
+        {
+            A.CallTo(() => _diceRoller.RollDie()).Returns(fakedDie);
+            using var assertionScope = new AssertionScope();
 
+            var result = _sut.Move(previousPosition);
+            result.Position.Should().Be(expected);
+
+        }
+
+        [TestCase(9, true)]
+        [TestCase(0, false)]
+        public void Move_Should_Return_GameStatus(int position, bool expected)
+        {
+            A.CallTo(() => _diceRoller.RollDie()).Returns(5);
+            var result = _sut.Move(position);
+            result.IsFinalSquareReached.Should().Be(expected);
         }
 
     }
--- SnakesAndLaddersLibTests/RollDiceAndMoveTests.cs
+++ SnakesAndLaddersLibTests/RollDiceAndMoveTests.cs
@@ -7,14 +7,7 @@ namespace SnakesAndLaddersLibTests
 {
     public class RollDiceAndMoveTests
     {
-        private IDiceRoller _diceRoller;
-        private PieceMover _sut;
-        [SetUp]
-        public void SetUp()
-        {
-            _diceRoller = A.Fake<IDiceRoller>();
-            _sut = new PieceMover(_diceRoller);
-        }
+
         [Test]
         public void SetUpTest()
         {
@@ -65,30 +58,5 @@ namespace SnakesAndLaddersLibTests
             var result = PieceMover.CalculatePosition(previousPosition, diceRoll);
             result.Should().Be(expected);
         }
-
-
-        [TestCase(0, 1, 1)]
-        [TestCase(5, 2, 7)]
-        [TestCase(5, 5, 10)]
-        [TestCase(7, 6, 10)]
-        public void Move_Should_Return_PositionInRange(int previousPosition, int fakedDie, int expected)
-        {
-            A.CallTo(() => _diceRoller.RollDie()).Returns(fakedDie);
-            using var assertionScope = new AssertionScope();
-            
-                var result = _sut.Move(previousPosition);
-                result.Position.Should().Be(expected);
-            
-        }
-
-        [TestCase(9, true)]
-        [TestCase(0, false)]
-        public void Move_Should_Return_GameStatus(int position, bool expected)
-        {
-            A.CallTo(() => _diceRoller.RollDie()).Returns(5);
-            var result = _sut.Move(position);
-            result.IsFinalSquareReached.Should().Be(expected);
-        }
-
     }
 }
