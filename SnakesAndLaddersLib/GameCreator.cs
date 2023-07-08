69ed00bce7e1d122f47715d48a234050bd6d3f3f
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-08 18:31:56 +0200

    green: Test CreateGame Should Not Be Null
    add return type Game to CreateGame - method

--- SnakesAndLaddersLib/GameCreator.cs
+++ SnakesAndLaddersLib/GameCreator.cs
@@ -2,9 +2,11 @@
 {
     public class GameCreator
     {
-        public static object CreateGame()
+        public static Game CreateGame()
         {
-            return null;    
+            return new Game(default, default, default, default);    
         }
     }
+
+    public record Game(bool DisabledDie, bool Status, int Position, int NumberOfFields);
 }
