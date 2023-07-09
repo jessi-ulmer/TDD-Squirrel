1bfce823fbbb75cc8ea4e902200fe4b99a043666
Yvonne Spitznagel <Yvonne.Spitznagel@generic.de>
2023-07-09 16:53:36 +0200

    Implement and Styling - Game preparation - Game end notification - Style cube button

--- TDD-Squirrel/Pages/Game.razor.cs
+++ TDD-Squirrel/Pages/Game.razor.cs
@@ -5,26 +5,47 @@ namespace TDD_Squirrel.Pages
     public partial class Game
     {
         private PieceMover pieceMover;
+
+        private int NumberOfFields = 10;
+        private int NumberOfRows = 3;
+
+        private List<List<bool>> playingFields;
+        private List<int> test;
+
+        private int PiecePosition = 1;
+        private bool DisabledDie = false;
+
         public Game()
         {
             var diceRoller = new DiceRoller();
             pieceMover = new PieceMover(diceRoller);
+            playingFields = new List<List<bool>>();
+            test = new List<int>();
         }
 
-        public int NumberOfFields = 10;
-        private int NumberOfRows = 3;
+        private void CreateGame()
+        {
+            playingFields = new List<List<bool>>().ToList();
+            for (var index = 0; index < NumberOfRows; index++)
+            {
+                Console.WriteLine(index);
 
-        public int PiecePosition = 1;
+                var fields = new bool[NumberOfFields];
+                playingFields.Add(new List<bool> { false });
 
-        public bool DisabledDie = false;
+                test.Add(index);
+            }
+
+            Console.WriteLine("####");
+            Console.WriteLine(playingFields);
+            StateHasChanged();
+        }
 
         private void MovePiece()
         {
-
             var result = pieceMover.Move(PiecePosition);
             DisabledDie = result.IsFinalSquareReached;
             PiecePosition = result.Position;
-
         }
     }
 }
