6cba59ea3b235887529f171c26cbef14a2370b8c
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-14 21:14:59 +0200

    refactor: delete old test

--- SnakesAndLaddersLibTests/GameCreatorTests.cs
+++ SnakesAndLaddersLibTests/GameCreatorTests.cs
@@ -1,40 +1,23 @@
-using FakeItEasy;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using SnakesAndLaddersLib;
+
 namespace SnakesAndLaddersLibTests;
 
 public class GameCreatorTests
 {
+    [TestCase(1, 1, 1)]
+    [TestCase(2, 2, 2)]
+    public void CrateGame_Should_Return_SquareGame(int size, int expectedRows, int expectedColumns)
+    {
+        var game = GameCreator.CreateGame(size);
 
+        using var scope = new AssertionScope();
+        game.Should().NotBeNull();
+        game.IsDieDisabled.Should().BeFalse();
+        game.Status.Should().BeTrue();
 
-        [TestCase(10)]
-        [TestCase(15)]
-        public void CreateGame_Should_Return_InitializedGame_ForGivenNumberOfFields(int numberOfFields)
-        {
-            var game = GameCreator.CreateGame(numberOfFields);
-
-            using var scope = new AssertionScope();
-            game.Should().NotBeNull();
-            game.IsDieDisabled.Should().BeFalse();
-            game.Status.Should().BeTrue();
-
-        }
-
-        [TestCase(1, 1,1)]
-        [TestCase(2, 2, 2)]
-        public void CrateGame_Should_Return_SquareGame(int size, int expectedRows, int expectedColumns)
-        {
-            var game = GameCreator.CreateGame(size);
-
-            using var scope = new AssertionScope();
-            game.Should().NotBeNull();
-            game.IsDieDisabled.Should().BeFalse();
-            game.Status.Should().BeTrue();
-
-            game.Rows.Should().Be(expectedRows);
-            game.Columns.Should().Be(expectedColumns);
-
-        }
+        game.Rows.Should().Be(expectedRows);
+        game.Columns.Should().Be(expectedColumns);
+    }
 }
-    
