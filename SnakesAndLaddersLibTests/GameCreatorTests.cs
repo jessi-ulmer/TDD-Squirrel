61a8a385a454a5e23cc593a5e49c66583d9ab757
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-14 17:15:31 +0200

    not building: add test for sqare game
    keep single line game test for comparison

--- SnakesAndLaddersLibTests/GameCreatorTests.cs
+++ SnakesAndLaddersLibTests/GameCreatorTests.cs
@@ -21,5 +21,19 @@ namespace SnakesAndLaddersLibTests;
             game.Position.Should().Be(0);
             game.NumberOfFields.Should().Be(numberOfFields);
         }
+
+        [Test]
+        public void CreateGame_Should_Return_SquareGame()
+        {
+            var game = GameCreator.CreateGame(1);
+
+            using var scope = new AssertionScope();
+            game.Should().NotBeNull();
+            game.Status.Should().BeTrue();
+            game.Position.Should().Be(0);
+            game.NumberOfFields.Should().Be(1);
+            game.Rows.Should().Be(1);
+            game.Columns.Should().Be(1);
+    }
     }
     
