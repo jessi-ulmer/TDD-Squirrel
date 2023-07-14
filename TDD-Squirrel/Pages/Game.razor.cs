9c2d35b159d3b87e9d1f6c8eeb4d843099158739
Yvonne Spitznagel <Yvonne.Spitznagel@generic.de>
2023-07-14 12:49:19 +0200

    Fix merge errors

--- TDD-Squirrel/Pages/Game.razor.cs
+++ TDD-Squirrel/Pages/Game.razor.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices.JavaScript;
-using SnakesAndLaddersLib;
+﻿using SnakesAndLaddersLib;
 
 namespace TDD_Squirrel.Pages
 {
@@ -7,54 +6,34 @@ namespace TDD_Squirrel.Pages
     {
         private PieceMover pieceMover;
 
-        private int NumberOfFields = 10;
-        private int NumberOfRows = 3;
-
-        private List<List<bool>> playingFields;
-        private List<int> test;
+        private int NumberOfFields = 4;
+        private int NumberOfRows = 1;
 
         private int PiecePosition = 1;
         private bool DisabledDie = false;
+        private bool ShowGame = false;
 
         public Game()
         {
             var diceRoller = new DiceRoller();
             pieceMover = new PieceMover(diceRoller);
-            playingFields = new List<List<bool>>();
-            test = new List<int>();
-        }
-
-        private void CreateGame()
-        {
-            playingFields = new List<List<bool>>().ToList();
-            for (var index = 0; index < NumberOfRows; index++)
-            {
-                Console.WriteLine(index);
-
-                var fields = new bool[NumberOfFields];
-                playingFields.Add(new List<bool> { false });
-
-                test.Add(index);
-            }
-
-            Console.WriteLine("####");
-            Console.WriteLine(playingFields);
-            StateHasChanged();
         }
 
         private void MovePiece()
         {
-            var result = pieceMover.Move(PiecePosition);
+            var result = pieceMover.Move(PiecePosition, NumberOfFields);
             DisabledDie = result.IsFinalSquareReached;
             PiecePosition = result.Position;
         }
         private void StartNewGame()
         {
-            var game = GameCreator.CreateGame(15);
+            var game = GameCreator.CreateGame(NumberOfFields);
             DisabledDie = game.IsDieDisabled;
             ShowGame = game.Status;
             PiecePosition = game.Position;
             NumberOfFields = game.NumberOfFields;
+
+            StateHasChanged();
         }
     }
 }
