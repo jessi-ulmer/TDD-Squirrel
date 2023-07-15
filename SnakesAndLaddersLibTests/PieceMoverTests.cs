f33b60e24e202a5755101755a648d8693adcffd2
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-15 17:15:34 +0200

    red: add three steps

--- SnakesAndLaddersLibTests/PieceMoverTests.cs
+++ SnakesAndLaddersLibTests/PieceMoverTests.cs
@@ -123,6 +123,7 @@ public class PieceMoverTests
         yield return new TestCaseData((0, 1), 1, (0, 0));
         yield return new TestCaseData((1, 0), 2, (0, 1));
         yield return new TestCaseData((1, 1), 2, (0, 0));
+        yield return new TestCaseData((1, 0), 3, (0, 0));
     }
 
 
