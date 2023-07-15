9c0b76b059b8a8292ada51e6d1e9c4a3bfd21a75
Dorothea Kintz <dorothea.kintz@generic.de>
2023-07-15 18:09:03 +0200

    red: add test for stepping up in size 3 square

--- SnakesAndLaddersLibTests/PieceMoverTests.cs
+++ SnakesAndLaddersLibTests/PieceMoverTests.cs
@@ -155,6 +155,7 @@ public class PieceMoverTests
         yield return new TestCaseData((1, 0), 3, 2, 2, (0, 0));
         yield return new TestCaseData((2, 0), 1, 3, 3, (2, 1));
         yield return new TestCaseData((2, 0), 2, 3, 3, (2, 2));
+        yield return new TestCaseData((2, 2), 1, 3, 3, (1, 2));
     }
 
 
