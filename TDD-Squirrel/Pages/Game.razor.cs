4a2213db65c8f5b0b07a65cb759e09bb2bfc3630
Yvonne Spitznagel <Yvonne.Spitznagel@generic.de>
2023-07-19 15:41:17 +0200

    Do things with Thea

--- TDD-Squirrel/Pages/Game.razor.cs
+++ TDD-Squirrel/Pages/Game.razor.cs
@@ -9,6 +9,7 @@ namespace TDD_Squirrel.Pages
 
         //Muss noch in eine extra Komponente ausgelagert werden
         private int _columnsCountPreparation = 1;
+        //ToDo: Rauswerfen - brauchen wir nicht!
         private int _rowsCountPreparation = 1;
 
         private int _columnsCount = 4;
@@ -27,24 +28,29 @@ namespace TDD_Squirrel.Pages
 
         private void MovePiece()
         {
-            var result = _pieceMover.Move(_piecePosition, _rowsCountPreparation, _columnsCountPreparation);
+            var result = _pieceMover.Move(_piecePosition, _rowsCount, _columnsCount);
             _disabledDie = result.IsFinalSquareReached;
             _piecePosition = result.Position;
+
+            Console.WriteLine(result.Position);
+
+            StateHasChanged();
         }
         private void StartNewGame()
         {
-            var game = GameCreator.CreateGame(_columnsCount);
+            var game = GameCreator.CreateGame(_columnsCountPreparation);
             _disabledDie = game.IsDieDisabled;
             _showGame = game.Status;
             _columnsCount = game.Columns;
             _rowsCount = game.Rows;
 
             // Vorerst selbst, soll aber noch von CreateGame kommen
-            _piecePosition = new Position(0, game.Columns - 1);
+            _piecePosition = new Position(0, game.Rows - 1);
 
             StateHasChanged();
         }
 
+        // Todo: Remove
         private void OnIsPlayingFieldSquareValueChanged(ChangeEventArgs e)
         {
             _isPlayingFieldSquare = (bool)(e.Value ?? false);
