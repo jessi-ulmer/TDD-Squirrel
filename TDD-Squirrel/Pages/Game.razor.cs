cbbe3556db328f45c05ab03f6008830d42d927d5
Yvonne Spitznagel <Yvonne.Spitznagel@generic.de>
2023-07-19 17:01:24 +0200

    Remove game preparation with rows and check if is a square game field
    We have currently only square games

--- TDD-Squirrel/Pages/Game.razor.cs
+++ TDD-Squirrel/Pages/Game.razor.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using SnakesAndLaddersLib;
+﻿using SnakesAndLaddersLib;
 
 namespace TDD_Squirrel.Pages
 {
@@ -7,14 +6,10 @@ namespace TDD_Squirrel.Pages
     {
         private readonly PieceMover _pieceMover;
 
-        //Muss noch in eine extra Komponente ausgelagert werden
-        private int _columnsCountPreparation = 1;
-        //ToDo: Rauswerfen - brauchen wir nicht!
-        private int _rowsCountPreparation = 1;
+        private int _preparationPayingFileSize = 1;
 
         private int _columnsCount = 4;
         private int _rowsCount = 1;
-        private bool _isPlayingFieldSquare;
 
         private Position _piecePosition = default!;
         private bool _disabledDie;
@@ -31,14 +26,11 @@ namespace TDD_Squirrel.Pages
             var result = _pieceMover.Move(_piecePosition, _rowsCount, _columnsCount);
             _disabledDie = result.IsFinalSquareReached;
             _piecePosition = result.Position;
-
-            Console.WriteLine(result.Position);
-
-            StateHasChanged();
         }
+
         private void StartNewGame()
         {
-            var game = GameCreator.CreateGame(_columnsCountPreparation);
+            var game = GameCreator.CreateGame(_preparationPayingFileSize);
             _disabledDie = game.IsDieDisabled;
             _showGame = game.Status;
             _columnsCount = game.Columns;
@@ -48,16 +40,6 @@ namespace TDD_Squirrel.Pages
             _piecePosition = new Position(0, game.Rows - 1);
 
             StateHasChanged();
-        }
-
-        // Todo: Remove
-        private void OnIsPlayingFieldSquareValueChanged(ChangeEventArgs e)
-        {
-            _isPlayingFieldSquare = (bool)(e.Value ?? false);
-
-            _rowsCount = _isPlayingFieldSquare
-                ? 0
-                : 1;
         }
     }
 }
